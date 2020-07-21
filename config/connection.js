7f19391c7276e07cf7bdada90660a728e5de2194
sam <moha0477@umn.edu>
2020-07-21 18:44:25 -0500

    fixed connection issue

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 const util = require('util');
 
+var connection;
+
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
-const connection = mysql.createConnection({
+ connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
